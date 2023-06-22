e61652a78f604727f8e3af8577a60f254a799ab3
erwan-blancard <erwan.blancard@laplateforme.io>
2023-06-22 13:15:10 +0200

    commit calculatrice avec pourcentage

--- src/main/java/calc_u_later/calc_u_later/Calc_U_Later.java
+++ src/main/java/calc_u_later/calc_u_later/Calc_U_Later.java
@@ -2,18 +2,12 @@ package calc_u_later.calc_u_later;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Calc_U_Later extends Application {
 
